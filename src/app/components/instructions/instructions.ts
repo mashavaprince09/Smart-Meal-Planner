d63f7405d0b9c2f5ec3dc63bcbc67c9747f11d72
Prince Mashava <mashavaprince09@gmail.com>
2026-01-13 20:50:54 +0200

    Add logic to instructions component

--- src/app/components/instructions/instructions.ts
+++ src/app/components/instructions/instructions.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {RecipeSearchService} from '../../services/recipe-search-service';
+import {InstructionsService} from '../../services/instructions-service';
+import {InstructionsRequest} from '../../models/requests/instructions-request';
+import {InstructionsResponse} from '../../models/responses/instructions-response';
 
 @Component({
   selector: 'app-instructions',
@@ -11,14 +13,27 @@ import {RecipeSearchService} from '../../services/recipe-search-service';
 export class Instructions implements OnInit {
 
   recipeId!: number;
-  instructions?: string;
+  instructions: InstructionsResponse[] = [];
 
-  constructor(
-    private route: ActivatedRoute,
-    private recipeService: RecipeSearchService
-  ) {}
+  constructor(private route: ActivatedRoute, private instructionsService: InstructionsService, private cdr: ChangeDetectorRef) {
+
+  }
 
   ngOnInit(): void {
     this.recipeId = Number(this.route.snapshot.paramMap.get('id'));
+    const instructionsRequest: InstructionsRequest= {'id':this.recipeId, 'stepBreakdown': true};
+
+    console.log('viewInstructions', this.recipeId);
+
+    this.instructionsService.getInstructions(instructionsRequest).subscribe({
+      next:(response) => {
+        this.instructions = response;
+        this.cdr.detectChanges();
+        console.log(this.instructions);
+      },
+      error: (error) => console.log("Instructions not found",error),
+    });
+
   }
+
 }
