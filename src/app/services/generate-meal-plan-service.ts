94fcc1d5288863f26e267eb05fab737d7d48b70a
Prince Mashava <mashavaprince09@gmail.com>
2026-01-15 22:05:35 +0200

    Add HTTP client integration to GenerateMealPlanService

--- src/app/services/generate-meal-plan-service.ts
+++ src/app/services/generate-meal-plan-service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import {GenerateMealPlanResponse} from '../models/responses/generate-meal-plan-response';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {GenerateMealPlanRequest} from '../models/requests/generate-meal-plan-request';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class GenerateMealPlanService {
-  
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  generateMealPlan(generateMealPlanRequest: GenerateMealPlanRequest): Observable<GenerateMealPlanResponse>{
+    const url = `${environment.apiBaseUrls.spoonacular}/mealplanner/generate`;
+    console.log('GenerateMealPlanService generateMealPlan', url);
+    return this.httpClient.get<GenerateMealPlanResponse>(url, {
+      params: {
+        apiKey: environment.apiKeys.spoonacular,
+        timeFrame: generateMealPlanRequest.timeFrame,
+        targetCalories: generateMealPlanRequest.targetCalories,
+        diet: generateMealPlanRequest.diet,
+        exclude: generateMealPlanRequest.exclude
+      }
+    })
+  }
+
 }
