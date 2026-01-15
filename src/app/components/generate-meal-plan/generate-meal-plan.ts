48442fc29141b294eb7a0dbdc46356ce8b6f1ba4
Prince Mashava <mashavaprince09@gmail.com>
2026-01-15 23:27:06 +0200

    Update generate-meal-plan logic to handle day/week time frames and responses

--- src/app/components/generate-meal-plan/generate-meal-plan.ts
+++ src/app/components/generate-meal-plan/generate-meal-plan.ts
@@ -1,7 +1,9 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {GenerateMealPlanService} from '../../services/generate-meal-plan-service';
-import {GenerateMealPlanResponse} from '../../models/responses/generate-meal-plan-response';
+import {GenerateMealPlanDayResponse} from '../../models/responses/generate-meal-plan-day-response';
 import {GenerateMealPlanRequest} from '../../models/requests/generate-meal-plan-request';
+import {FormsModule} from '@angular/forms';
+import {GenerateMealPlanWeekResponse} from '../../models/responses/generate-meal-plan-week-response';
 
 @Component({
   selector: 'app-generate-meal-plan',
@@ -11,25 +13,45 @@ import {GenerateMealPlanRequest} from '../../models/requests/generate-meal-plan-
 })
 export class GenerateMealPlan implements OnInit{
 
-  generateMealPlanResponse!: GenerateMealPlanResponse;
-  generateMealPlanRequest: GenerateMealPlanRequest = {timeFrame: 'day', targetCalories: 2000, diet: 'vegetarian', exclude: ''};
+  generateMealPlanDayResponse: GenerateMealPlanDayResponse | null = null;
+  generateMealPlanWeekResponse: GenerateMealPlanWeekResponse | null = null;
+  generateMealPlanRequest: GenerateMealPlanRequest = {timeFrame: '', targetCalories: 1000, diet: '', exclude: ''};
 
   constructor(private generateMealPlanService: GenerateMealPlanService, private cdr: ChangeDetectorRef) {
 
   }
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit(){
     console.log('Generating meal plan...', this.generateMealPlanRequest);
-    this.generateMealPlanService.generateMealPlan(this.generateMealPlanRequest).subscribe({
-      next: (response) => {
-        this.generateMealPlanResponse = response;
-        this.cdr.detectChanges();
-        console.log('Meal plan generated:', this.generateMealPlanResponse);
-      },
-      error: (error) => {
-        console.error('Error generating meal plan:', error);
-      }
-    });
+    if (this.generateMealPlanRequest.timeFrame === 'day') {
+      this.generateMealPlanService.generateMealDayPlan(this.generateMealPlanRequest).subscribe({
+        next: (response) => {
+          this.generateMealPlanDayResponse = response;
+          this.cdr.detectChanges();
+          console.log('Meal plan generated:', this.generateMealPlanDayResponse);
+        },
+        error: (error) => {
+          console.error('Error generating meal plan:', error);
+        }
+      });
+    }
+    else {
+      this.generateMealPlanService.generateMealWeekPlan(this.generateMealPlanRequest).subscribe({
+        next: (response) => {
+          this.generateMealPlanWeekResponse = response;
+          this.cdr.detectChanges();
+          console.log('Meal plan generated:', this.generateMealPlanWeekResponse);
+        },
+        error: (error) => {
+          console.error('Error generating meal plan:', error);
+        }
+      });
+    }
+
   }
 
 
