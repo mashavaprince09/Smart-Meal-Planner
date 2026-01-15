6cc1ab442a60deacfd76b2a726a361443577b651
Prince Mashava <mashavaprince09@gmail.com>
2026-01-15 22:07:53 +0200

    Add downstream call to generate-meal-plan-service

--- src/app/components/generate-meal-plan/generate-meal-plan.ts
+++ src/app/components/generate-meal-plan/generate-meal-plan.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {GenerateMealPlanService} from '../../services/generate-meal-plan-service';
+import {GenerateMealPlanResponse} from '../../models/responses/generate-meal-plan-response';
+import {GenerateMealPlanRequest} from '../../models/requests/generate-meal-plan-request';
 
 @Component({
   selector: 'app-generate-meal-plan',
@@ -6,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './generate-meal-plan.html',
   styleUrl: './generate-meal-plan.css',
 })
-export class GenerateMealPlan {
+export class GenerateMealPlan implements OnInit{
+
+  generateMealPlanResponse!: GenerateMealPlanResponse;
+  generateMealPlanRequest: GenerateMealPlanRequest = {timeFrame: 'day', targetCalories: 2000, diet: 'vegetarian', exclude: ''};
+
+  constructor(private generateMealPlanService: GenerateMealPlanService, private cdr: ChangeDetectorRef) {
+
+  }
+
+  ngOnInit(): void {
+    console.log('Generating meal plan...', this.generateMealPlanRequest);
+    this.generateMealPlanService.generateMealPlan(this.generateMealPlanRequest).subscribe({
+      next: (response) => {
+        this.generateMealPlanResponse = response;
+        this.cdr.detectChanges();
+        console.log('Meal plan generated:', this.generateMealPlanResponse);
+      },
+      error: (error) => {
+        console.error('Error generating meal plan:', error);
+      }
+    });
+  }
+
 
 }
